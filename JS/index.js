347856b6fa11a5f8a1272b966aadc7b26289947f
ben23515225566 <ben23515225566@gmail.com>
2024-03-28 21:37:35 +0800

    Vue verison

--- JS/index.js
+++
@@ -1,7 +0,0 @@
-function Goto(id){
-    document.querySelector(id).scrollIntoView({
-        block: "start", // 決定了垂直捲軸的位置
-        inline: "nearest", // 決定水平捲軸的位置
-        behavior: "smooth"
-    });
-}
